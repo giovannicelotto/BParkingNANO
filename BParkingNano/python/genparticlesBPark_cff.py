cf5d6fb897abdd174d386fd9200b344f94d27247
Giovanni Celotto <giovanni.celottog@gmail.com>
2023-12-06 18:41:34 +0100

    save genpart from higgs

--- BParkingNano/python/genparticlesBPark_cff.py
+++ BParkingNano/python/genparticlesBPark_cff.py
@@ -10,7 +10,8 @@ finalGenParticlesBPark = finalGenParticles.clone(
   src = cms.InputTag("mergedGenParticles"),
   select = cms.vstring(
 	"drop *",
-        "++keep++ (abs(pdgId) == 511 || abs(pdgId) == 521)",  #keep all B0(=511) and B+/-(521) + their daughters and granddaughters
+        "++keep++ (abs(pdgId) == 25)",  # "++keep++ (abs(pdgId) == 511 || abs(pdgId) == 521)", keep all B0(=511) and B+/-(521) + their daughters and granddaughters
+        "++keep++ (abs(pdgId) == 23)",
    )
 )
 
